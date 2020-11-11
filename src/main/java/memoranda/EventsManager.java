6a45f5e6a57b27d653e7d1e5741d52960d1ebeb7
iswanlun <iswanlun@asu.edu>
2020-11-10 23:30:03 -0800

    Debug messages removed

--- src/main/java/memoranda/EventsManager.java
+++ src/main/java/memoranda/EventsManager.java
@@ -190,8 +190,6 @@ public class EventsManager {
 			//System.out.println(date.inPeriod(ev.getStartDate(),
 			// ev.getEndDate()));
 			if (date.inPeriod(ev.getStartDate(), ev.getEndDate())) {
-	/*DEBUG TESTING*/
-				System.out.println("CAll to repeat in events manager.");
 				if (ev.getRepeat() == REPEAT_DAILY) {
 					int n = date.getCalendar().get(Calendar.DAY_OF_YEAR);
 					int ns =
