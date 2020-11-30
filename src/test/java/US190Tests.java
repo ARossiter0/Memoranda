493149ec32eb034a35f8252ad6c7b4066813b00d
Tristan Johnson <collar50@gmail.com>
2020-11-30 23:47:20 +0800

    Setup checkstyle and spotbugs, and correct unit tests

--- src/test/java/US190Tests.java
+++ src/test/java/US190Tests.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import main.java.memoranda.CurrentProject;
 import main.java.memoranda.Project;
@@ -19,17 +19,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Test Name: US190_Tests.java
- *
+ * Test Name: US190Tests.java
+ * <p></p>
  * Test Description: Test the methods changed significantly in US190.
- *
+ * <p></p>
  * @Author Tristan Johnson
  * @Date 11/30/2020
  */
-public class US190_Tests {
+public class US190Tests {
 
     @Before
-    public void setup(){
+    public void setup() {
 
     }
 
@@ -37,20 +37,22 @@ public class US190_Tests {
      * Test getStatus method in TaskImpl.java.
      */
     @Test
-    public void testGetStatus(){
+    public void testGetStatus() {
         // Create task
         CalendarDate projectStartDate = new CalendarDate(10, 8, 2020);
         CalendarDate projectEndDate = new CalendarDate(10, 2, 2021);
-        Project project = ProjectManager.createProject("Project 1", projectStartDate, projectEndDate);
+        Project project = ProjectManager.createProject("Project 1",
+                projectStartDate, projectEndDate);
         TaskList taskList = new TaskListImpl(project);
         CalendarDate taskStartDate = new CalendarDate(10, 11, 2020);
         CalendarDate taskEndDate = new CalendarDate(10, 12, 2020);
-        Task task = taskList.createTask(taskStartDate, taskEndDate, "Task 1", 1, 3, "The first task", null);
+        Task task = taskList.createTask(taskStartDate, taskEndDate, "Task 1",
+                1, 3, "The first task", null);
 
         // Create dates
-        CalendarDate date1 = new CalendarDate(9, 12, 2020);
-        CalendarDate date6 = new CalendarDate(9, 11, 2020);
-        CalendarDate date7 = new CalendarDate(11, 12, 2020);
+        final CalendarDate date1 = new CalendarDate(9, 12, 2020);
+        final CalendarDate date6 = new CalendarDate(9, 11, 2020);
+        final CalendarDate date7 = new CalendarDate(11, 12, 2020);
 
         // Check task status
         assertEquals(Task.ACTIVE, task.getStatus(taskStartDate));
@@ -64,11 +66,12 @@ public class US190_Tests {
      * Test getChild method in TaskTableSorter.java.
      */
     @Test
-    public void testGetChild(){
+    public void testGetChild() {
         // Create tasks
         CalendarDate projectStartDate = new CalendarDate(10, 8, 2020);
         CalendarDate projectEndDate = new CalendarDate(10, 2, 2021);
-        Project project = ProjectManager.createProject("Project 1", projectStartDate, projectEndDate);
+        Project project = ProjectManager.createProject("Project 1",
+                projectStartDate, projectEndDate);
         TaskList taskList = new TaskListImpl(project);
 
         CalendarDate taskStartDate1 = new CalendarDate(10, 10, 2020);
@@ -77,11 +80,21 @@ public class US190_Tests {
         CalendarDate taskEndDate2 = new CalendarDate(10, 9, 2020);
         CalendarDate taskStartDate3 = new CalendarDate(10, 1, 2021);
         CalendarDate taskEndDate3 = new CalendarDate(10, 2, 2021);
-        Task task1 = taskList.createTask(taskStartDate1, taskEndDate1, "US190-Test Task 1", 1, 3, "The first task", null);
-        Task task2 = taskList.createTask(taskStartDate2, taskEndDate2, "US190-Test Task 2", 1, 3, "The first task", null);
-        Task task3 = taskList.createTask(taskStartDate1, taskEndDate1, "US190-Test Task 3", 1, 3, "The first task", null);
-        Task task4 = taskList.createTask(taskStartDate3, taskEndDate3, "US190-Test Task 4", 1, 3, "The first task", null);
-        Task task5 = taskList.createTask(taskStartDate1, taskEndDate1, "US190-Test Task 5", 1, 3, "The first task", null);
+        final Task task1 = taskList.createTask(taskStartDate1, taskEndDate1,
+                "US190"
+                + "-Test Task 1", 1, 3, "The first task", null);
+        final Task task2 = taskList.createTask(taskStartDate2, taskEndDate2,
+                "US190"
+                + "-Test Task 2", 1, 3, "The first task", null);
+        final Task task3 = taskList.createTask(taskStartDate1, taskEndDate1,
+                "US190"
+                + "-Test Task 3", 1, 3, "The first task", null);
+        final Task task4 = taskList.createTask(taskStartDate3, taskEndDate3,
+                "US190"
+                + "-Test Task 4", 1, 3, "The first task", null);
+        final Task task5 = taskList.createTask(taskStartDate1, taskEndDate1,
+                "US190"
+                + "-Test Task 5", 1, 3, "The first task", null);
 
         Storage storage = CurrentStorage.get();
         storage.storeTaskList(taskList, project);
